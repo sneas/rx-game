4445d16b60d00dcee72c62e7a86e8a944924311e
Dima Snisarenko <snisarenkodima@gmail.com>
2018-09-24 15:02:48 +0200

    Calculate displace with scan

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,19 @@
 import { createCanvas, drawPixels } from "./draw";
 import { fromEvent, of } from "rxjs";
 import { Pixel } from "./types";
-import { filter } from "rxjs/operators";
+import { filter, scan, startWith } from "rxjs/operators";
 
 const canvas = createCanvas();
 const ctx = canvas.getContext("2d");
 document.body.appendChild(canvas);
 
 fromEvent(document, "keydown")
-  .pipe(filter(e => e["keyCode"] === 38))
-  .subscribe(event => {
-    console.log(event);
+  .pipe(
+    filter(e => e["keyCode"] === 38),
+    scan((displace: number, _: Event) => displace + 1, 0)
+  )
+  .subscribe(displace => {
+    console.log(displace);
   });
 
 of<Pixel[]>([
