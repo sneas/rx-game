592989c74167c98528cc1732b570b2f0d220b6b3
Dima Snisarenko <snisarenkodima@gmail.com>
2018-09-24 15:52:19 +0200

    Jump on key up

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,7 @@
 import { clear, createCanvas, drawPixels } from "./draw";
 import { fromEvent, interval, of, zip } from "rxjs";
 import { Pixel } from "./types";
-import { filter, map, scan, startWith } from "rxjs/operators";
-import { clean } from "./scene";
+import { exhaustMap, filter, map, scan } from "rxjs/operators";
 import { fromArray } from "rxjs/internal/observable/fromArray";
 
 const canvas = createCanvas();
@@ -10,24 +9,26 @@ const ctx = canvas.getContext("2d");
 document.body.appendChild(canvas);
 
 fromEvent(document, "keydown")
-  .pipe(filter(e => e["keyCode"] === 38))
-  .subscribe(event => {
-    console.log(event);
-  });
-
-zip(fromArray([1, 1, 1, 1, 1, 1, -1, -1, -1, -1, -1, -1]), interval(50))
   .pipe(
-    map(([displace]) => displace),
-    scan(
-      (pixels: Pixel[], displace: number) => {
-        return pixels.map(pixel => ({ ...pixel, y: pixel.y + displace }));
-      },
-      [
-        { x: 2, y: 0, color: "black" },
-        { x: 2, y: 1, color: "green" },
-        { x: 2, y: 2, color: "blue" }
-      ]
-    )
+    filter(e => e["keyCode"] === 38),
+    exhaustMap(() => {
+      return zip(
+        fromArray([1, 1, 1, 1, 1, 1, -1, -1, -1, -1, -1, -1]),
+        interval(50)
+      ).pipe(
+        map(([displace]) => displace),
+        scan(
+          (pixels: Pixel[], displace: number) => {
+            return pixels.map(pixel => ({ ...pixel, y: pixel.y + displace }));
+          },
+          [
+            { x: 2, y: 0, color: "black" },
+            { x: 2, y: 1, color: "green" },
+            { x: 2, y: 2, color: "blue" }
+          ]
+        )
+      );
+    })
   )
   .subscribe(pixels => {
     clear(ctx); // Fills the entire scene with blue rectangle
