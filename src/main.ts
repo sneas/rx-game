b71fcfb78a6b1217e1d6b241f3cf1c3572c2980a
Dima Snisarenko <snisarenkodima@gmail.com>
2018-09-24 14:34:04 +0200

    Draw some pixels

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,15 @@
-console.log(`It's working!`);
+import { createCanvas, drawPixels } from "./draw";
+import { of } from "rxjs";
+import { Pixel } from "./types";
+
+const canvas = createCanvas();
+const ctx = canvas.getContext("2d");
+document.body.appendChild(canvas);
+
+of<Pixel[]>([
+  { x: 2, y: 0, color: "black" },
+  { x: 2, y: 1, color: "green" },
+  { x: 2, y: 2, color: "blue" }
+]).subscribe(pixels => {
+  drawPixels(ctx, pixels);
+});
